89a4960a6977b4713ac8818d5d36b10e050b85d3
Rogério Neves dos Santos <103578702+RogerNSantos@users.noreply.github.com>
2023-07-18 17:48:05 -0300

    Add files via upload
    *Objetivo: Desenvolver um programa em JavaScript que faça a Leitura de 2 números inteiros
            e apresente o resultado da soma entre eles.*

---
+++ Somatoria_parseInt.js
@@ -0,0 +1,15 @@
+
+/* Definição das Variáveis */
+var num1;
+var num2;
+var soma;
+
+/* Entrada de Dados */
+num1 = parseInt(window.prompt('Informe o primeiro número: '));
+num2 = parseInt(window.prompt('Informe o segundo número: '));
+
+/* Processamento de Dados */
+soma = (num1) + (num2);
+
+/* Saída de Dados */
+document.write('O Resultado da soma é = '+ soma);
